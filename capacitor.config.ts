6f799cba1fa7bac161e1f6e65ebfab919e7e612c
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-21 14:52:18 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'FirsApp',
+  webDir: 'www'
+};
+
+export default config;
